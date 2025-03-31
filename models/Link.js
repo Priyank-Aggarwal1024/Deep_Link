a4ff7db5e605375256e1359ce19c37ea4651ac97
Mukul-Rustagi <adityarustagi1507@gmail.com>
2025-03-31 08:38:10 +0530

    done

--- models/Link.js
+++ models/Link.js
@@ -3,9 +3,10 @@ const shortid = require("shortid");
 
 const linkSchema = new mongoose.Schema({
   shortId: { type: String, default: shortid.generate, unique: true },
-  longURL: { type: String, required: true },
-  deepLink: { type: String },
-  userType: { type: String, enum: ["customer", "supplier", "organization"] },
+  longURL: { type: String, required: true, unique: true },
+  androidLink: { type: String },
+  iosLink: { type: String },
+  userType: { type: String, enum: ["customer", "supplier", "organization", "default"] },
   createdAt: { type: Date, default: Date.now },
 });
 
